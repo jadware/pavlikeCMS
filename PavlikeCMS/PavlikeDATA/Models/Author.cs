a969b0144765f2133ffa8b8f14a2e56ee42334df
Uğurhan Gül <ugurhangul@hotmail.com>
2015-07-06 16:27:54 +0300

    .

--- PavlikeCMS/PavlikeDATA/Models/Author.cs
+++ PavlikeCMS/PavlikeDATA/Models/Author.cs
@@ -10,15 +10,16 @@ namespace PavlikeDATA.Models
     {
         public int Id { get; set; }
         public string UserGuid { get; set; }
+        public string RoleGuid { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
         public DateTime DateofBirth { get; set; }
         public string EMail { get; set; }
+
         public virtual ICollection<Page> Pages { get; set; }
         public virtual ICollection<Article> Articles { get; set; }
         public virtual ICollection<File> Files { get; set; }
         public virtual ICollection<Album> Albums { get; set; }
-
         public virtual ICollection<Media> Media { get; set; }
 
     }
